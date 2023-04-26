a14cc1d9aaaa1ab0a54bfc6a909d10180f826950
Dhruval <dhruvalbhuva2000@gmail.com>
2023-04-26 17:56:17 +0200

    Update

--- open_mobile_camera.py
+++ open_mobile_camera.py
@@ -1,12 +1,13 @@
-import cv2
+import cv2, pafy
 
+#'''
 #connect your laptop and android device with same network either wifi or hotspot
 # add this IP into browser to open on lptop
 cameraIP = "https://192.168.0.15:8080/video"
 
 #Here parameter 0 is a path of any video use for webcam
-cap = cv2.VideoCapture(0)
-cap.open(cameraIP)
+cap = cv2.VideoCapture(cameraIP)
+#cap.open()
 
 print("Cam check:", cap.isOpened())
 
@@ -33,3 +34,36 @@ while cap.isOpened():
 cap.release()
 outputObj.release()
 cv2.destroyAllWindows()
+#'''
+
+'''
+# Capture video from youtube
+youtubeURL = "https://www.youtube.com/watch?v=SLD9xzJ4oeU"
+
+data = pafy.new(youtubeURL)
+data = data.getbest(preftype="mp4")
+
+cap = cv2.VideoCapture(data.url, cv2.CAP_DSHOW)
+
+print("Cam check:", cap.isOpened())
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret == True:
+        frame = cv2.resize(frame, (700, 600))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('Colorframe', frame)
+        cv2.imshow("Gray Frame", gray)
+
+        if cv2.waitKey(1) == ord("q"):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+'''
