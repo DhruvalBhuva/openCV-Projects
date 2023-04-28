2348b9faf2f84f28aca9dd82f8c3865b510b3964
Dhruval <dhruvalbhuva2000@gmail.com>
2023-04-28 12:29:33 +0200

    Update

--- Video_read_write.py
+++ Video_read_write.py
@@ -2,19 +2,27 @@ import cv2
 
 '''=>  Read video and open it '''
 
-'''
-video = cv2.VideoCapture("D:\Image\Dhruval\video.mp4")
-print("video", video)
+# '''
+# cap = cv2.VideoCapture("D:\Image\Dhruval\video.mp4") # for local video
+cap = cv2.VideoCapture(0)  # for camera
+print("video", cap)
 
 while True:
-    ret, frame = video.read()  # read() returen ret  as True if frame is avilable
-    print("Frame", frame
+    ret, frame = cap.read()  # read() returen ret  as True if frame is avilable
+    print("Frame", frame)
 
-    print("Width: ", cv2.CAP_PROP_FRAME_WIDTH)
-    print("Height: ", cv2.CAP_PROP_FRAME_HEIGHT)
+    print("Width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Int value for cv2.CAP_PROP_FRAME_WIDTH is 3
+    print("Height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Int value for cv2.CAP_PROP_FRAME_HEIGHT is 4
     # for More properties: https://docs.opencv.org/4.0.0/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
 
-    frame = cv2.resize(frame, (700, 450))
+    # Set height and width: it will set value as per camera resolution
+    cap.set(3, 700)
+    cap.set(4, 700)
+
+    print("Width: ", cap.get(3))
+    print("Height: ", cap.get(4))
+
+    # frame = cap.resize(frame, (700, 450))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frameFlip = cv2.flip(frame, -1)
 
@@ -24,12 +32,12 @@ while True:
     if cv2.waitKey(25) & 0xFF == ord("q"):
         break
 
-video.release()
-cv2.destroyAllWindows() 
-'''
-
+cap.release()
+cv2.destroyAllWindows()
 # '''
-#=> Open video camera, record that video and save it '''
+
+''' 
+# => Open video camera, record that video and save it 
  
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)   #Here parameter 0 is a path of any video use for webcam
 print("checking..",cap.isOpened())
@@ -65,4 +73,4 @@ while cap.isOpened():
 cap.release()
 outputObj.release()
 cv2.destroyAllWindows()
-# '''
+'''
