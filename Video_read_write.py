7e1aecb51a753ab03e7393cd2aa65bfe14344a07
Dhruval <dhruvalbhuva2000@gmail.com>
2023-05-01 23:08:25 +0200

    Update write on video

--- Video_read_write.py
+++ Video_read_write.py
@@ -1,33 +1,46 @@
 import cv2
+import datetime
 
 '''=>  Read video and open it '''
 
 # '''
-# cap = cv2.VideoCapture("D:\Image\Dhruval\video.mp4") # for local video
-cap = cv2.VideoCapture(0)  # for camera
-print("video", cap)
+cap = cv2.VideoCapture("C:/Users/ASUS/Downloads/video.mp4",) # for local video
+# cap = cv2.VideoCapture(0)  # for camera
+
+# print("video", cap)
 
 while True:
     ret, frame = cap.read()  # read() returen ret  as True if frame is avilable
-    print("Frame", frame)
 
-    print("Width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Int value for cv2.CAP_PROP_FRAME_WIDTH is 3
-    print("Height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Int value for cv2.CAP_PROP_FRAME_HEIGHT is 4
-    # for More properties: https://docs.opencv.org/4.0.0/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
+    if ret == True:
+        print("Frame", frame)
 
-    # Set height and width: it will set value as per camera resolution
-    cap.set(3, 700)
-    cap.set(4, 700)
+        print("Width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Int value for cv2.CAP_PROP_FRAME_WIDTH is 3
+        print("Height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Int value for cv2.CAP_PROP_FRAME_HEIGHT is 4
+        # for More properties: https://docs.opencv.org/4.0.0/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
 
-    print("Width: ", cap.get(3))
-    print("Height: ", cap.get(4))
+        # Set height and width: it will set value as per camera resolution
+        # cap.set(3, 200)
+        # cap.set(4, 200)
+        # print("Width: ", cap.get(3))
+        # print("Height: ", cap.get(4))
 
-    # frame = cap.resize(frame, (700, 450))
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frameFlip = cv2.flip(frame, -1)
+        # Writing on the video
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text="Width:" + str(cap.get(3))+ ", Height:" + str(cap.get(4))
+        dateTime = str(datetime.datetime.now())
+        frame = cv2.putText(frame, dateTime, (10,50), font, 1, (0,255,255), 2, cv2.LINE_AA)
 
-    cv2.imshow("ColorFrame", frame)
-    cv2.imshow("Gray Frame", gray)
+        # frame = cap.resize(frame, (700, 450))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frameFlip = cv2.flip(frame, -1)
+
+        cv2.imshow("ColorFrame", frame)
+        # cv2.imshow("Gray Frame", gray)
+
+    else:
+        print("Error")
+        break
 
     if cv2.waitKey(25) & 0xFF == ord("q"):
         break
